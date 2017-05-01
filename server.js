381186cb78342d76ae70dcfd5d7e0cbfcca11063
Rgustave <mugustave02@gmail.com>
2017-05-01 14:49:25 -0400

    mongoose connection update

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser =  require('body-parser');
 
-mongoose.connect(process.env.MONGODB_URI);
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+process.env.MONGOLAB_PURPLE_URI||
+'mongodb://localhost/customerdb';
+
+
+var theport = process.env.PORT || 5000;
+
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,5 +28,5 @@ app.use(bodyParser.json());
 
 app.use('/api',require('./routes/api'))
 
-var server  = app.listen(process.env.PORT || 3000);
+var server  = app.listen(theport);
 console.log('API is running on port',server.address().port);
